865e7c7c633bf2d5250e807c1381ac7e1f06148d
Sup#2.0 <sup2point0@gmail.com>
2024-10-27 12:02:41 +0000

    sup

---
+++ src/parts/ui/nav.link.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+
+import { base } from "$app/paths";
+import { browser } from "$app/environment";
+
+export let text: string;
+export let intern: string;
+
+</script>
+
+
+<a class="part nav-link"
+  href="{base}/{intern}"
+  class:active={browser && window?.location.href == `${base}/${intern}`}
+>
+  {text}
+</a>
+
+
+<style lang="scss">
+
+.part {
+  margin: 0 0.5rem;
+  padding: 0.5em 1em;
+  display: inline-block;
+
+  @include font-ui;
+  color: white;
+  text-decoration: none;
+  background-color: transparent;
+
+  transition: background-color 0.2s ease-out;
+
+  &.hover {
+    color: $col-prot;
+    background-color: $col-card;
+  }
+
+  &:hover, &.active {
+    color: red;
+  }
+}
+
+</style>
---
+++ src/parts/ui/nav.svelte
@@ -0,0 +1,42 @@
+<script>
+
+import NavLink from "#parts/ui/nav.link.svelte";
+
+</script>
+
+
+<nav>
+  <NavLink
+    text="info"
+    intern="sup/info"
+  />
+  <NavLink
+    text="sup"
+    link="https://sup2point0.github.io"
+  />
+  <NavLink
+    text="loves"
+    intern="sup/loves"
+  />
+  <NavLink
+    text="music"
+    intern="sup/music"
+  />
+  <NavLink
+    text="projects"
+    intern="sup/projects"
+  />
+  <NavLink
+    text="dev"
+    intern="sup/dev"
+  />
+</nav>
+
+
+<style lang="scss">
+
+nav {
+  margin: 1rem;
+}
+
+</style>

abba690b62deeb6434a84a88d17f7c0399a95766
Sup#2.0 <sup2point0@gmail.com>
2024-10-27 15:36:43 +0000

    sup

---
+++ src/parts/ui/back.svelte
@@ -0,0 +1,27 @@
+<!-- @component Background -->
+<script lang="ts">
+
+export let style: "violet" | "scarlet" = "scarlet";
+
+</script>
+
+
+<img class="part back"
+  src="/ui/cortex-{style}-vert.jpg"
+/>
+
+
+<style lang="scss">
+
+.part {
+  max-width: 100%;
+  max-height: none;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: -1;
+  filter: blur(4px) brightness(0.88);
+  transform: translateZ(-20px) scale(2);
+}
+
+</style>

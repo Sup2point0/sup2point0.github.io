e97327236f3a421db4da3b4708cb8e1ab33a4b01
Sup#2.0 <sup2point0@gmail.com>
2024-10-28 20:13:26 +0000

    sup

---
+++ src/parts/content-card.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+
+export let title: string = "";
+export let pict: string | undefined = undefined;
+export let link: string | undefined = undefined;
+  export let intern: string | undefined = undefined;
+  export let extern: string | undefined = undefined;
+export let side: "left" | "right" = "left";
+
+</script>
+
+
+<a class="content-card"
+  target={extern ? "_blank" : "_self"}
+  href="{extern ?? link ?? ``}
+>
+  <div class="content-inner"
+    style:flex-direction={side == "right" ? "row-reverse" : "row"}
+  >
+    {#if pict}
+      <img alt={title} href={pict} />
+    {/if}
+
+    <div>
+      <h3> {title} </h3>
+      <slot />
+    </div>
+  </div>
+</a>
+
+
+<style lang="scss">
+
+.content-card {
+  margin: 0;
+  padding: 0.5rem;
+  display: block;
+  background: rgba(black, 60%);
+
+  &:hover {}
+
+  &:active {}
+}
+
+.content-inner {
+  display: flex;
+}
+
+</style>

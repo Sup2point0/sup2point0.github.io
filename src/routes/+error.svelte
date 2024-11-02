e2763a7a49ce44205c1d171f2f590d5ff8c63e89
Sup#2.0 <sup2point0@gmail.com>
2024-11-02 18:53:20 +0000

    sup

---
+++ src/routes/+error.svelte
@@ -0,0 +1,32 @@
+<main>
+  <p> Uh, something’s gone wrong! </p>
+</main>
+
+
+<style lang="scss">
+
+:global(html, body) {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: black;
+}
+
+main {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  background-color: black;
+}
+
+p {
+  font-family: system-ui, sans-serif;
+  color: white;
+  text-align: center;
+}
+
+</style>

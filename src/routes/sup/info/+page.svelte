ee38371230fe0265d53daaa39ffacd4d2e8c23ec
Sup#2.0 <sup2point0@gmail.com>
2024-11-05 15:03:09 +0000

    sup

---
+++ src/routes/sup/info/+page.svelte
@@ -0,0 +1,56 @@
+<table>
+  <tbody>
+    <tr>
+      <th> username </th>
+      <td> Sup#2.0 / Sup2.0 / Sup2point0 </td>
+    </tr>
+    <tr>
+      <th> pronouns </th>
+      <td> he / him </td>
+    </tr>
+    <tr>
+      <th> locale </th>
+      <td> Antarctica </td>
+    </tr>
+    <tr>
+      <th> timezone </th>
+      <td> UTC </td>
+    </tr>
+    <tr>
+      <th> language </th>
+      <td> English (British) / Chinese (Mandarin) </td>
+    </tr>
+    <tr>
+      <th> cake day </th>
+      <td> September </td>
+    </tr>
+    <tr>
+      <th> generation </th>
+      <td> Z </td>
+    </tr>
+  </tbody>
+</table>
+
+<p style:text-align="center"> thank you for stalking me, it’s been my pleasure ^v^ </p>
+
+
+<style lang="scss">
+
+* {
+  // border: 1px solid lightskyblue;
+}
+
+table {
+
+}
+
+tr {
+  text-align: left;
+}
+
+th {
+  padding: 0.5em 2rem 0.5em 0.5em;
+  color: rgb(white, 60%);
+}
+
+</style>

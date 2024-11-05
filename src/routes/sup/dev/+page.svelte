84d553ff1332fd033ddea63e1b23649f84699843
Sup#2.0 <sup2point0@gmail.com>
2024-11-05 08:15:52 +0000

    sup

--- src/routes/sup/dev/+page.svelte
+++ src/routes/sup/dev/+page.svelte
@@ -5,9 +5,8 @@ import Words from "#parts/words.svelte";
 </script>
 
 
-<section>
-  <h2> I develop in </h2>
-
+<section class="left">
+  <h2> I code in </h2>
   <div class="words">
     <Words words={[
       "Python", "C#", "Ruby",
@@ -16,8 +15,48 @@ import Words from "#parts/words.svelte";
   </div>
 </section>
 
+<section class="right">
+  <h2> alongside </h2>
+  <div class="words">
+    <Words words={[
+      "Svelte", "SvelteKit",
+      "Markdown", "JSON", "JSON Schema", "YAML",
+      "SQL", "LaTeX", "RegEx",
+    ]} />
+  </div>
+</section>
+
+<section class="left">
+  <h2> and I develop in </h2>
+  <div class="words">
+    <Words words={[
+      "VSCode", "GitHub Codespaces",
+      "Unity", "Desmos",
+      "GarageBand",
+      "PowerPoint",
+      "MolView",
+    ]} />
+  </div>
+</section>
+
+<section class="right">
+  <h2> I’d like to learn </h2>
+  <div class="words">
+    <Words words={[
+      "Swift", "Julia", "Carbon", "Elixir"
+    ]} />
+  </div>
+</section>
+
 
 <style lang="scss">
+
+section {
+  margin-bottom: 4rem;
+
+  &.left { text-align: left; }
+  &.right { text-align: right; }
+}
 
 .words {
   display: flex;

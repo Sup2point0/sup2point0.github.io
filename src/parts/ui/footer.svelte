67e2ed48da069dc85c6f8bc80e7a0f85c591307c
Sup#2.0 <sup2point0@gmail.com>
2024-11-05 08:13:42 +0000

    sup

---
+++ src/parts/ui/footer.svelte
@@ -0,0 +1,29 @@
+<!-- @component Footer -->
+
+<footer>
+  <p> crafted with ❤️ by Sup#2.0 </p>
+  <p> built with <a target="_blank" href="https://svelte.dev">Svelte 5</a> + <a target="_blank" href="https://svelte.dev/docs/kit/introduction">SvelteKit</a> </p>
+  <p> hosted on <a target="_blank" href="https://pages.github.com/">GitHub Pages</a> </p>
+</footer>
+
+
+<style lang="scss">
+
+@use './src/styles/links' as *;
+
+
+footer {
+  margin: 1rem;
+  padding: 0.5em;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 1em;;
+}
+
+p {
+  @include font-flavour;
+}
+
+</style>

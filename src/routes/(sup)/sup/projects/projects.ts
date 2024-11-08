fbe8da600bae3ea6d354038f32b54aa3175c469f
Sup#2.0 <sup2point0@gmail.com>
2024-11-08 18:22:37 +0000

    sup

---
+++ src/routes/(sup)/sup/projects/projects.ts
@@ -0,0 +1,105 @@
+export const projects = [
+
+  { "title": "Assort",
+    "rank": 1,
+    "desc": "An assortment of all my creations. Like a personal wiki!",
+    "tags": ["Markdown", "Svelte", "SvelteKit"],
+    "flags": ["personal, dev"]
+  },
+  { "title": "supcode",
+    "rank": 2,
+    "desc": "A conceptual programming language with flavourful syntax",
+    "tags": ["Markdown", "supcode"],
+    "flags": ["personal"]
+  },
+  { "title": "Antarctica",
+    "rank": 2,
+    "desc": "A fictional reimaging of the coolest place on Earth.",
+    "tags": ["Markdown"],
+    "flags": ["personal"]
+  },
+  { "title": "Victory",
+    "rank": 3,
+    "desc": "A custom card game for me and my sister to play, inspired by <em>Yu-Gi-Oh!</em>",
+    "tags": ["Markdown", "HTML", "CSS", "JavaScript"],
+    "flags": ["personal, dev"]
+  },
+  { "title": "The Monkeyopolis Times",
+    "rank": -1,
+    "desc": "A fan-made Bloons TD 6 newspaper",
+    "tags": ["Google Slides", "reddit"],
+    "flags": ["personal, depr"]
+  },
+  { "title": "Avidity",
+    "rank": 2,
+    "desc": "A web-based music player for personal use",
+    "tags": ["Svelte", "SvelteKit"],
+    "flags": ["dev, active"]
+  },
+  { "title": "Stranger Quarkdown",
+    "rank": 2,
+    "desc": "An automated content deployment framework for SvelteKit",
+    "tags": ["Ruby"],
+    "flags": ["dev"]
+  },
+  { "title": "pyco:bytes",
+    "rank": 2,
+    "desc": "A weekly newsletter on Python tips and tricks",
+    "tags": ["Markdown", "Svelte", "SvelteKit"],
+    "flags": ["dev"]
+  },
+  { "title": "Vividity",
+    "rank": 3,
+    "desc": "Provides colour palettes for use in other projects",
+    "tags": ["Python"],
+    "flags": ["dev"]
+  },
+  { "title": "WeightedList",
+    "rank": 3,
+    "desc": "A specialised list for weighted randomisation, implemented in every language as I know. I now use it as an exercise when learning new ones!",
+    "tags": ["Python", "C#", "JavaScript", "Ruby"],
+    "flags": ["dev"]
+  },
+  { "title": "suptools",
+    "rank": 3,
+    "desc": "Utilities for general use throughout Python projects",
+    "tags": ["Python"],
+    "flags": ["dev"]
+  },
+  { "title": "youtube-playlist-shuffler",
+    "rank": 3,
+    "desc": "A script for shuffling a YouTube playlist through the Google API",
+    "tags": ["Python"],
+    "flags": ["dev"]
+  },
+  { "title": "Algorhythm",
+    "rank": -1,
+    "desc": "A keyboard-based rhythm game. Originally implemented in Python as a proof of concept, will properly develop in Unity someday!",
+    "tags": ["Python", "pygame"],
+    "flags": ["dev, depr"]
+  },
+  { "title": "Ignis",
+    "rank": -2,
+    "desc": "A series of projects relating to Yu-Gi-Oh",
+    "tags": ["Python", "Keras"],
+    "flags": ["dev, depr"]
+  },
+  { "title": "PENGUIN",
+    "rank": -1,
+    "desc": "",
+    "tags": ["Python", "Nextcord"],
+    "flags": ["dev, depr"]
+  },
+  { "title": "Quarkdown",
+    "rank": -3,
+    "desc": "An automated content deployment framework for exporting "Markdown" files in repositories to GitHub Pages",
+    "tags": ["Python"],
+    "flags": ["dev, depr"]
+  },
+  { "title": "REAX",
+    "rank": -3,
+    "desc": "A probabilistic particle simulator for modelling chemical reactions",
+    "tags": ["C#", "Unity"],
+    "flags": ["dev, depr"]
+  },
+];

adc89185cd61dc3a50808f1e92cfc6a8ad63d955
Sup#2.0 <sup2point0@gmail.com>
2024-11-08 19:32:58 +0000

    sup

---
+++ src/routes/(sup)/sup/loves/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+
+import Words from "#parts/words.svelte";
+
+</script>
+
+
+<section class="left">
+  <h2> I spend my time </h2>
+  <div class="words">
+    <Words words={[]} />
+  </div>
+</section>
+
+
+<style lang="scss">
+
+section {
+  margin-bottom: 4rem;
+}
+
+.words {
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+
+</style>

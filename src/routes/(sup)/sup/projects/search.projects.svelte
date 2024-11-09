6fb88c796f5ba10f2340d1ceca4bb0e0840178d3
Sup#2.0 <sup2point0@gmail.com>
2024-11-09 12:04:47 +0000

    sup

--- src/routes/(sup)/sup/projects/search.projects.svelte
+++ src/routes/(sup)/sup/projects/search.projects.svelte
@@ -7,13 +7,22 @@ import { search_data } from "./search-data.svelte.ts";
 </script>
 
 
-<input class="search"
-  type="search"
-  bind:value={search_data.query}
-/>
+<div class="search-projects">
+  <input class="search"
+    type="search"
+    bind:value={search_data.query}
+  />
+</div>
 
 
 <style lang="scss">
+
+div.search-projects {
+  margin: 1rem 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
 
 input.search {
   min-width: 50%;

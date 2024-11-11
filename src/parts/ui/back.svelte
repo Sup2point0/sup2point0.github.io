6f755d8c9e62ee935bdd8f82ebbf2e1f69892621
Sup#2.0 <sup2point0@gmail.com>
2024-11-11 18:22:44 +0000

    sup

--- src/parts/ui/back.svelte
+++ src/parts/ui/back.svelte
@@ -9,32 +9,32 @@ let {
 } = $props();
 
 
-let page: HTMLElement | null;
-let back: HTMLElement | null;
+// let page: HTMLElement | null;
+// let back: HTMLElement | null;
 
-let window_height: number | undefined = $derived(browser ? window.innerHeight : undefined);
-let page_height: number | undefined = $derived(page?.scrollHeight);
-let back_height: number | undefined;
+// let window_height: number | undefined = $derived(browser ? window.innerHeight : undefined);
+// let page_height: number | undefined = $derived(page?.scrollHeight);
+// let back_height: number | undefined;
 
-let scroll_frac: number = $state(0);
-let scroll_offset: number = $state(0);
+// let scroll_frac: number = $state(0);
+// let scroll_offset: number = $state(0);
 
 
-onMount(() => {
-  if (browser) {
-    page = document?.getElementById("page");
-    back = document?.getElementById("back");
-    back_height = back?.scrollHeight;
+// onMount(() => {
+//   if (browser) {
+//     page = document?.getElementById("page");
+//     back = document?.getElementById("back");
+//     back_height = back?.scrollHeight;
 
-    page?.addEventListener("scroll", () => {
-      let scroll = page!.scrollTop;
+//     page?.addEventListener("scroll", () => {
+//       let scroll = page!.scrollTop;
 
-      if (scroll && page_height && window_height) {
-        scroll_frac = scroll / (page_height - window_height);
-      }
-    })
-  }
-})
+//       if (scroll && page_height && window_height) {
+//         scroll_frac = scroll / (page_height - window_height);
+//       }
+//     })
+//   }
+// })
 
 </script>
 
@@ -42,8 +42,8 @@ onMount(() => {
 <img id="back"
   alt="cortex-{col}"
   src="/ui/cortex-{col}-vert.jpg"
-  style:top="{Math.round(scroll_frac * (window_height - back_height))}px"
 />
+  <!-- style:top="{Math.round(scroll_frac * (window_height - back_height))}px" -->
 
 
 <style lang="scss">
@@ -55,8 +55,7 @@ onMount(() => {
   left: 0;
   z-index: -1;
   filter: blur(4px) brightness(0.88);
-  // transform: scale(1.05);
-  // transition: top 0.1s ease-out;
+  transform: scale(1.05);
 }
 
 </style>

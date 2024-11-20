c63050323f01261524dbee33fa797fc2a6b4141c
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-11-20 22:38:49 +0000

    sup

--- src/routes/(sup)/sup/music/+page.svelte
+++ src/routes/(sup)/sup/music/+page.svelte
@@ -14,20 +14,9 @@ import Album from "#parts/music/album-block.svelte";
   <Album
     name="Singles"
     year="2023 – present"
+    tracks={1}
     intern="singles"
     cover="singles/sunset.png"
-  />
-  <Album
-    name="GarageBand Archives"
-    year="2024 and earlier"
-    intern="archives/garageband"
-    cover="garageband-archives.jpg"
-  />
-  <Album
-    name="MuseScore Archives"
-    year="2023 and earlier"
-    intern="archives/musescore"
-    cover="musescore-archives.png"
   />
 </div>
 
@@ -35,50 +24,81 @@ import Album from "#parts/music/album-block.svelte";
   <Album
     name="Algorhythm Origins"
     year="2023 – present"
+    tracks={8}
     intern="algo-origins"
     cover="algo-origins/night.png"
   />
   <Album
     name="Algorhythm Roots"
     year="2024 – present"
+    tracks={4}
     intern="algo-roots"
     cover="algo-roots/rejuvenate.png"
   />
   <Album
     name="Algorhythm Vision"
     year="2024 – future"
+    tracks={4}
     intern="algo-vision"
     cover="algo-vision/another-dawn.png"
   />
   <Album
     name="ELYSION"
     year="2023 – present"
+    tracks={7}
     intern="elysion"
     cover="elysion/ceruleus.png"
   />
   <Album
     name="Cortex"
     year="2024 – present"
+    tracks={8}
     intern="cortex"
     cover="cortex/voxel.png"
   />
   <Album
     preview={true}
+    name="Integral"
+    year="2024 – present"
+    intern="integral"
+  />
+  <Album
+    preview={true}
+    name="Affinity"
+    tracks={9}
+    year="2024 – present"
+    intern="affinity"
+  />
+  <Album
+    preview={true}
     name="Fidelity"
+    tracks={7}
     year="future"
     intern="fidelity"
   />
   <Album
     preview={true}
     name="Stranded"
+    tracks={6}
     year="future"
     intern="stranded"
   />
+</div>
+
+<div class="collection">
   <Album
-    preview={true}
-    name="Integral"
-    year="2024 – present"
-    intern="Integral"
+    name="GarageBand Archives"
+    year="2024 and earlier"
+    tracks={4}
+    intern="archives/garageband"
+    cover="garageband-archives.jpg"
+  />
+  <Album
+    name="MuseScore Archives"
+    year="2023 and earlier"
+    tracks={3}
+    intern="archives/musescore"
+    cover="musescore-archives.png"
   />
 </div>
 

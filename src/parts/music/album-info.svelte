ce20acb9ffe5efe53e2da595eb9c544796581cd7
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-11-20 22:55:38 +0000

    sup

--- src/parts/music/album-info.svelte
+++ src/parts/music/album-info.svelte
@@ -1,9 +1,15 @@
-<!-- @component AlbumInfo -->
+<!-- @component AlbumInfo
+
+A block containing title and description of an album.
+-->
+
 <script lang="ts">
 
-export let daw: "gb" | "ms" | "ms-4" = "gb";
+export let title: string = ""
+export let daw: "fl" | "gb" | "ms" | "ms-4" = "gb";
 
 const DAW = {
+  fl: "FL Studio",
   gb: "GarageBand (iPad)",
   ms: "MuseScore 3",
   "ms-4": "MuseScore 4",
@@ -13,7 +19,10 @@ const DAW = {
 
 
 <div class="part album-info">
-  <slot> Something went wrong! </slot>
+  <h1> {title} </h1>
+  <p>
+    <slot> ... </slot>
+  </p>
 
   <p class="daw"> Made in {DAW} </p>
 </div>
@@ -30,8 +39,17 @@ const DAW = {
 }
 
 
-p.daw {
-  text-align: right;
+h1 {
+  @include font-ui;
+}
+
+p {
+  @include font-ui;
+
+  &.daw {
+    @include font-flavour;
+    text-align: right;
+  }
 }
 
 </style>

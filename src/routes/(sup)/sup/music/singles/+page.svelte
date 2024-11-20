723bf085217c869c0ea0d6a5304f99e186f3b29f
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-11-20 23:22:10 +0000

    sup

--- src/routes/(sup)/sup/music/singles/+page.svelte
+++ src/routes/(sup)/sup/music/singles/+page.svelte
@@ -9,9 +9,19 @@ import Tags from "#parts/music/tags.svelte";
 
 <AlbumInfo daw="gb">
   <h1> Singles </h1>
+
   <Tags tags={["electronic"]} />
 </AlbumInfo>
 
+<Track
+  preview={true}
+  feat={true}
+  shard="rise"
+  name="Rise"
+  tags={["vocals", "melodramatic"]}
+  album="singles"
+  file="ride.m4a"
+/>
 <Track
   shard="sunset"
   name="Sunset"

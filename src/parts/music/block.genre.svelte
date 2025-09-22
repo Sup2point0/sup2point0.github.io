659467908760fad165cb6f9a0de9e530ee7c0a00
Sup#2.0 <sup2point0@gmail.com>
2025-09-22 11:50:14 +0100

    sup

--- src/parts/music/block.genre.svelte
+++ src/parts/music/block.genre.svelte
@@ -17,8 +17,10 @@ let { genre }: Props = $props();
 
 
 let self: HTMLElement;
+
 let intersected = $state(false);
 let left = $state(0);
+let top = $state(0);
 
 onMount(() => {
   let observer = new IntersectionObserver(entries => {
@@ -26,6 +28,7 @@ onMount(() => {
       if (!intersected && entry.isIntersecting) {
         intersected = true;
         left = entry.boundingClientRect.left;
+        top = entry.boundingClientRect.top;
       }
     }
   }, {
@@ -43,21 +46,23 @@ onMount(() => {
   class:fav={genre.fav}
   id={genre.name.toLowerCase()}
   bind:this={self}
-  style:transition-delay="calc(({left}px / 100vw) * 420ms)"
+  style:--delay="calc(({left}px / 100vw) * 420ms + ({top}px / 100vh) * 200ms)"
 >
-  <div class="upper">
-    <h3> {genre.name} </h3>
-  </div>
+  <div class="content">
+    <div class="upper">
+      <h3> {genre.name} </h3>
+    </div>
 
-  <div class="lower">
-    {#each genre.artists ?? [] as artist}
-      <!-- TODO find a better way to access shard -->
-      <a href="artists#{artist.toLowerCase()}">
-        {artist}
-      </a>
+    <div class="lower">
+      {#each genre.artists ?? [] as artist}
+        <!-- TODO find a better way to access shard -->
+        <a href="artists#{artist.toLowerCase()}">
+          {artist}
+        </a>
 
-      <span class="separator"> × </span>
-    {/each}
+        <span class="separator"> × </span>
+      {/each}
+    </div>
   </div>
 </button>
 
@@ -71,26 +76,42 @@ button.block.genre {
   flex-grow: 1;
   max-width: 32rem;
   padding: 1rem 1.5rem;
+  background: none;
+  border: none;
+  @include shear-card($interactive: true);
+
+  &::before {
+    transform: skew($shear-factor) scale(90%);
+    opacity: 0;
+    transition:
+      #{trans()},
+      transform 1s cubic-bezier(0.19, 1, 0.22, 1) var(--delay, 0s),
+      opacity 1s cubic-bezier(0.19, 1, 0.22, 1) var(--delay, 0s);  // ease-out-exp
+  }
+
+  &.intersected::before {
+    transform: skew($shear-factor);
+    opacity: 1;
+  }
+
+  &:hover {
+    cursor: auto;
+  }
+}
+
+.content {
+  width: 100%;
   display: flex;
   flex-flow: column nowrap;
   justify-content: space-between;
   align-items: start;
   gap: 1rem;
-
-  background: none;
-  border: none;
-  @include shear-card($interactive: true);
-
   transform: scale(90%);
   opacity: 0;
-  transition: all 1s cubic-bezier(0.19, 1, 0.22, 1);  // ease-out-exp
+  transition: all 1s cubic-bezier(0.19, 1, 0.22, 1) var(--delay, 0s);  // ease-out-exp
 
-  &:hover {
-    cursor: auto;
-  }
-
-  &.intersected {
-    transform: scale(100%);
+  button.block.genre.intersected & {
+    transform: none;
     opacity: 1;
   }
 }

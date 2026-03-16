e7864d445e9ced44609cdac14fee75accb89d3e7
Sup#2.0 <sup2point0@gmail.com>
2026-03-16 23:28:08 +0000

    sup

--- src/scripts/anim.svelte.ts
+++ src/scripts/anim.svelte.ts
@@ -1,39 +1,28 @@
-export class AnimationData
+import { expoInOut } from "svelte/easing";
+
+
+export const expoSlide = { duration: 800, easing: expoInOut };
+
+export function anim(node: HTMLElement)
 {
-  intersected: boolean = $state(false);
-  left: number = 0;
-  top: number = 0;
-}
-
-
-export function anim(node: HTMLElement): void
-{
-  let anim = new AnimationData();
-
-  register_animation(node, anim);
-
-  /* @ts-ignore */
-  node.style["--delay"] = calc_delay(anim, 0.2);
-}
-
-
-/** DEPRECATED */
-export function register_animation(root: HTMLElement, anim: AnimationData)
-{
-  if (!root) {
+  if (!node) {
     console.warn("failed to attach animation handler!");
     return;
   };
+
+  let intersected = false;
   
   let observer = new IntersectionObserver(
     entries => {
       for (let entry of entries)
       {
-        if (!anim.intersected && entry.isIntersecting) {        
+        if (!intersected && entry.isIntersecting) {        
           entry.target.classList.add("intersected");
-          anim.intersected = true;
-          anim.left = entry.boundingClientRect.left;
-          anim.top = entry.boundingClientRect.top;
+
+          let left = entry.boundingClientRect.left;
+          let top = entry.boundingClientRect.top;
+          let delay = calc_delay(left, top);
+          node.style.setProperty("--delay", delay);
         }
       }
     },
@@ -42,16 +31,16 @@ export function register_animation(root: HTMLElement, anim: AnimationData)
     }
   );
 
-  observer.observe(root);
+  observer.observe(node);
 
-  return () => observer.unobserve(root);
+  return () => observer.unobserve(node);
 }
 
-/** DEPRECATED */
-export function calc_delay(anim: AnimationData, scale: number = 1): string
+
+function calc_delay(left: number, top: number, scale: number = 1): string
 {
   return `calc(
-      (${anim.left}px / 100vw) * ${300 * scale}ms
-    + (${anim.top}px / 100vh) * ${100 * scale}ms
+      (${left}px / 100vw) * ${200 * scale}ms
+    + (${top}px / 100vh) * ${100 * scale}ms
   )`;
 }

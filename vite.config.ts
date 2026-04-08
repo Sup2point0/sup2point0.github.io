4d216e993609ee02f5174195dc39b41aaee71b14
Sup#2.0 <sup2point0@gmail.com>
2026-04-08 15:56:02 +0100

    sup

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 
+if (process.argv.includes("--open")) {
+  process.env.BROWSER = "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe";
+}
+
+
 export default defineConfig({
   plugins: [sveltekit()],
-  server: {
-    open: process.env.BROWSER = "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe",
-  },
 });

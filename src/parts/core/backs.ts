b21cd9f97854c0fb5e93a4f3bc43af22598b7848
Sup#2.0 <sup2point0@gmail.com>
2026-04-14 13:54:39 +0100

    sup

--- src/parts/core/backs.ts
+++ src/parts/core/backs.ts
@@ -7,7 +7,7 @@ interface InterpData {
 }
 
 export interface PictData {
-  file:  string;
+  file:       string;
   scale?: InterpData;
   blur?: InterpData;
   brightness?: InterpData;
@@ -17,77 +17,96 @@ export interface PictData {
 export const backs = new FrozenWeightedList<PictData>
 (
   // standard
-  [
-    10, { file:  "cortex.scarlet.jpg",
-          blur:  { init: 4, end: 2 },
-          brightness: { init: 80, end: 100 } }
+  [ 20, {
+      file:       "cortex.scarlet.jpg",
+      blur:       { init: 4, end: 2 },
+      brightness: { init: 80, end: 100 }
+    }
   ],
-  [
-    10, { file:  "cortex.violet.jpg",
-          blur:  { init: 3, end: 1 },
-          brightness: { init: 94, end: 100 } }
+  [ 20, {
+      file:       "cortex.violet.jpg",
+      blur:       { init: 3, end: 1 },
+      brightness: { init: 94, end: 100 }
+    }
   ],
-  [
-    5, { file:  "sunset.png",
-          blur:  { init: 6, end: 4 },
-          brightness: { init: 80, end: 80 } }
+  [ 10, {
+      file:       "sunset.png",
+      blur:       { init: 6, end: 4 },
+      brightness: { init: 80, end: 80 }
+    }
   ],
 
   // variety
-  [
-    4, { file:  "geometric.cyber.jpg",
-          scale: { init: 160, end: 110 },
-          blur:  { init: 10, end: 4 },
-          brightness: { init: 40, end: 60 } }
+  [ 4, {
+      file:       "geometric.cyber.jpg",
+      scale:      { init: 160, end: 110 },
+      blur:       { init: 10, end: 4 },
+      brightness: { init: 40, end: 60 }
+    }
   ],
-  [
-    4, { file:  "cortex.blue.jpg",
-          blur:  { init: 6, end: 2 },
-          brightness: { init: 94, end: 100 } }
+  [ 4, {
+      file:       "cortex.blue.jpg",
+      blur:       { init: 6, end: 2 },
+      brightness: { init: 94, end: 100 }
+    }
   ],
-  [
-    4, { file:  "hex.blue.2.png",
-          scale: { end: 135 },
-          blur:  { init: 4, end: 2 },
-          brightness: { init: 40, end: 100 } }
+  [ 4, {
+      file:       "hex.blue.2.png",
+      scale:      { end: 135 },
+      blur:       { init: 4, end: 2 },
+      brightness: { init: 40, end: 100 }
+    }
   ],
-  [
-    4, { file:  "cortex.hex.blue.jpg",
-          scale: { init: 120, end: 150 },
-          blur:  { init: 6, end: 2 },
-          brightness: { init: 50, end: 80 } }
+  [ 4, {
+      file:       "cortex.hex.blue.jpg",
+      scale:      { init: 120, end: 150 },
+      blur:       { init: 6, end: 2 },
+      brightness: { init: 50, end: 80 }
+    }
   ],
 
   // flavour
-  [
-    5, { file:  "manifold.1.png",
-          scale: { end: 115 },
-          blur:  { init: 16, end: 2 },
-          brightness: { init: 50, end: 25 } }
+  [ 5, {
+      file:       "manifold.1.png",
+      scale:      { end: 115 },
+      blur:       { init: 16, end: 2 },
+      brightness: { init: 50, end: 25 }
+    }
   ],
-  [
-    5, { file:  "manifold.4.png",
-          scale: { end: 110 },
-          blur:  { init: 12, end: 2 },
-          brightness: { init: 40, end: 25 } }
+  [ 5, {
+      file:       "manifold.4.png",
+      scale:      { end: 110 },
+      blur:       { init: 12, end: 2 },
+      brightness: { init: 40, end: 25 }
+    }
+  ],
+  [ 5, {
+      file:       "wilds.1.jpg",
+      scale:      { init: 115, end: 105 },
+      blur:       { init: 3, end: 4 },
+      brightness: { init: 100, end: 90 }
+    }
+  ],
+  [ 3, {
+      file:       "firewatch.4.jpg",
+      scale:      { end: 145 },
+      blur:       { init: 7, end: 2 },
+      brightness: { init: 70, end: 80 }
+    }
   ],
 
   // special
-  [
-    3, { file:  "firewatch.4.jpg",
-          scale: { end: 145 },
-          blur:  { init: 7, end: 2 },
-          brightness: { init: 70, end: 80 } }
+  [ 2, {
+      file:       "drytron.jpg",
+      scale:      { end: 130 },
+      blur:       { init: 12, end: 4 },
+      brightness: { init: 40, end: 60 }
+    }
   ],
-  [
-    2, { file:  "drytron.jpg",
-          scale: { end: 130 },
-          blur:  { init: 12, end: 4 },
-          brightness: { init: 40, end: 60 } }
-  ],
-  [
-    1, { file:  "soul.necropolis.1.jpg",
-          blur:  { init: 4, end: 6 },
-          brightness: { init: 80, end: 50 } }
+  [ 1, {
+      file:       "soul.necropolis.1.jpg",
+      blur:       { init: 4, end: 6 },
+      brightness: { init: 80, end: 50 }
+    }
   ],
 );

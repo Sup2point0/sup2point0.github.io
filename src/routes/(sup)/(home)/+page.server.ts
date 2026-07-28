6739e3bb67a8a5df0cf1c76d5df9dee963ea6595
Sup#2.0 <sup2point0@gmail.com>
2026-07-28 21:40:36 +0100

    sup
    schedule deployment to weekly

--- src/routes/(sup)/(home)/+page.server.ts
+++ src/routes/(sup)/(home)/+page.server.ts
@@ -7,14 +7,16 @@ export interface VideoData
 
 export async function load()
 {
-  console.log("fetching YouTube videos");
+  console.log("fetching YouTube videos...");
 
   let response = await fetch("https://www.youtube.com/feeds/videos.xml?channel_id=UCymDd4idgL1slmKjA3L0NHQ");
 
-  console.log("received response")
+  console.log("received response!")
 
   let xml = await response.text();
   let chunks = xml.split("<entry>", 4);
+
+  console.log(`received ${chunks.length} chunks!`);
 
   let videos: VideoData[] = [];
 

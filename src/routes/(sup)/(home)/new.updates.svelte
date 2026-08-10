c5c6bdac04c3a5a2b2cd0e8aca556ef13c681941
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2026-08-10 22:44:07 +0100

    sup
    update

--- src/routes/(sup)/(home)/new.updates.svelte
+++ src/routes/(sup)/(home)/new.updates.svelte
@@ -16,22 +16,22 @@ import { fade } from "svelte/transition";
 
 const updates = [
   {
+    title: "Making a LaTeX to Desmos compiler!",
+    link: "https://github.com/Sup2point0/desmost",
+    project: "desmost",
+    desc: `Write LaTeX in code blocks alongside your Markdown, then compile it to a Desmos graphing calculator embed!`,
+  },
+  {
+    title: `Now installable as a Windows app!`,
+    link: `https://sup2point0.github.io/lattix`,
+    project: "lattix",
+    desc: `Tauri is a breeze`
+  },
+  {
     title: `Making a mathematical synthesiser!`,
     link: `https://sup2point0.github.io/awxynth`,
-    domain: "awxynth",
+    project: "awxynth",
     desc: `It uses Desmos (!)`
-  },
-  {
-    title: `Preferences are saved locally!`,
-    link: `https://sup2point0.github.io/lattix`,
-    domain: "lattix",
-    desc: `(finally!) took me long enough lol`
-  },
-  {
-    title: `Music player supports drag to seek!`,
-    link: `https://sup2point0.github.io`,
-    domain: `sup2point0.github.io`,
-    desc: `Head to ${a("music/create", "/sup/music/create", true)}, play any track of mine, and drag the playhead to move around playback`
   },
 ];
 
@@ -43,6 +43,7 @@ const updates = [
     [20, `what’s popping`],
     [20, `recent updates`],
     [10, `what I’ve been working on`],
+    [10, `new stuff`],
   ]} />
 </h2>
 
@@ -50,7 +51,7 @@ const updates = [
   {#each updates as update}
     <FactCard
       text={update.title}
-      capt={a(update.domain, update.link)}
+      capt={a(update.project, update.link)}
       capt_pos="right"
       desc={update.desc}
     />

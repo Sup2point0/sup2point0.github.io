c00874f4a1c55f35be3dcd88a9b4bcff0e27a93f
Sup#2.0 <sup2point0@gmail.com>
2026-09-01 11:39:31 +0100

    sup
    enlarge YouTube embeds

--- src/routes/(sup)/(home)/new.youtube.svelte
+++ src/routes/(sup)/(home)/new.youtube.svelte
@@ -25,7 +25,7 @@ import { page } from "$app/state";
 
   <Cards>
     {#each page.data.videos as id}
-      <iframe width="336" height="189" src="https://www.youtube.com/embed/{id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <iframe width="356" height="200" src="https://www.youtube.com/embed/{id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     {/each}
   </Cards>
 {/if}

997669bd38ff0042f2049661d3a2cfe730fc6fc2
Sup#2.0 <sup2point0@gmail.com>
2026-09-17 11:19:34 +0100

    sup
    use `a`, `i`, `h` with template literals, add `b`

--- src/scripts/utils/text.ts
+++ src/scripts/utils/text.ts
@@ -1,16 +1,25 @@
-/** Italicise text. */
-export function i(text: string): string
+/** Italicise `text` with `<em>`. */
+export function i(text: string | TemplateStringsArray): string
 {
-	return `<em>${text}</em>`;
+	let raw = Array.isArray(text) ? text.join(" ") : text;
+	return `<em>${raw}</em>`;
 }
 
-/** Highlight text. */
-export function h(text: string): string
+/** Bold `text` with `<strong>`. */
+export function b(text: string | TemplateStringsArray): string
 {
-	return `<span class="highlight">${text}</span>`;
+	let raw = Array.isArray(text) ? text.join(" ") : text;
+	return `<strong>${raw}</strong>`;
 }
 
-/** Add a hyperlink to text. */
+/** Highlight `text` with `<span class="highlight">`. */
+export function h(text: string | TemplateStringsArray): string
+{
+	let raw = Array.isArray(text) ? text.join(" ") : text;
+	return `<span class="highlight">${raw}</span>`;
+}
+
+/** Add a hyperlink to `text`. */
 export function a(text: string, href: string = "", intern: boolean = false): string
 {
 	return href ? `<a class="link" ${intern ? '' : 'target="_blank"'} href="${href}">${text}</a>` : text;
